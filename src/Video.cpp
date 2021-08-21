c3ed76293a7d8f2698428e131211843e67d46280
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-08-22 00:20:11 +0700

    support spacebar in output file name

--- src/Video.cpp
+++ src/Video.cpp
@@ -8,19 +8,29 @@
 
 void Video::linkAudio(std::string fileName)
 {
+    std::string safeFileName;
+    for (char c : fileName)
+    {
+        if (c == ' ')
+        {
+            safeFileName.push_back('\\');
+        }
+        safeFileName.push_back(c);
+    }
+
     // TODO Make Bitrate adjustable
-    std::string lowerBitrateCommand = "ffmpeg -y -i " + fileName +
-                                      ".avi -b:v 1024k -minrate 128k -maxrate 16M " + fileName +
+    std::string lowerBitrateCommand = "ffmpeg -y -i " + safeFileName +
+                                      ".avi -b:v 1024k -minrate 128k -maxrate 16M " + safeFileName +
                                       ".temp.avi";
 
     std::cout << "Executing: " << lowerBitrateCommand << "\n";
     std::system(lowerBitrateCommand.c_str());
 
     std::string linkAudioCommand("ffmpeg -y -i ");
-    linkAudioCommand += fileName + ".temp.avi -i " + RTX_MUSIC +
+    linkAudioCommand += safeFileName + ".temp.avi -i " + RTX_MUSIC +
                         " -b:a 384k -map 0:v:0 -map 1:a:0 -c:v "
                         "copy -shortest ";
-    linkAudioCommand += fileName + ".mp4";
+    linkAudioCommand += safeFileName + ".mp4";
 
     std::cout << "Executing: " << linkAudioCommand << "\n";
     std::system(linkAudioCommand.c_str());
