e98b7377abfa28954dcded58a8cf84d918e3c4f4
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-08-25 15:25:06 +0700

    version branch

--- src/AboutApp.cpp
+++ src/AboutApp.cpp
@@ -6,13 +6,17 @@
 
 std::string App::aboutAppSimple()
 {
-    return std::string("RTX 2090 TiFy  ") + APP_VERSION;
+    return std::string("RTX 2090 TiFy ") + APP_VERSION +
+           (VERSION_BRANCH == std::string("Release") ? ""
+                                                     : " (" + std::string(VERSION_BRANCH) + ")");
 }
 
 std::string App::aboutAppLong()
 {
     std::string AboutApp = "Turns you into RTX 2090 Ti\nApp Version: ";
     AboutApp += APP_VERSION;
+    AboutApp += "\nVersion Branch: ";
+    AboutApp += VERSION_BRANCH;
     AboutApp += "\nCreated by Leomotors\nUsing wxWidgets & OpenCV";
     return AboutApp;
 }
